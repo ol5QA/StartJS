586a83037268ac848be687016a1323a8d08c6794
ol5QA <118045680+ol5QA@users.noreply.github.com>
2025-08-01 09:11:22 +0300

    trying

--- 01_hello_user/index.ts
+++ 01_hello_user/index.ts
@@ -1,3 +1,3 @@
 // 01_hello_user/index.ts
 const name = process.argv[2] ?? 'StartSchool';   // берём имя из аргумента
-console.log(`Hello ${Olesja}!`);            // выводим приветствие
+console.log(`Hello ${StartSchool}!`);                   // печатаем "Hello <имя>!"
